3dfa613a987c18b1793a9703353b490680cae50c
Diksha <diksha1995@gmail.com>
2020-06-17 23:25:14 +0530

    apply validation

--- src/Components/screens/ForgotPassword/forgotpassword.js
+++ src/Components/screens/ForgotPassword/forgotpassword.js
@@ -16,82 +16,30 @@ export default class ForgotPassword extends Component {
         }
     }
     async onsubmit() {
-        const email = this.state.emaiId
-        const res = await api.fetchapi('http://180.149.241.208:3022/forgotPassword', 'post',
-            JSON.stringify({ "email": this.state.emailId }))
-        const result = await res.json();
-        console.log("api", result)
-        if (result.success === true) {
-
-            this.props.navigation.navigate('SetPassword', { otp: result.otp, token: result.token })
+        if (this.state.emailId == ' ') {
+            Alert.alert(" email id cannot be empty")
+        }
+        else if (this.state.email_err !== ' ') {
+            Alert.alert(" email  is invalid")
         }
         else {
-            console.log(result.json, " g")
-            Alert.alert("email_id is invalid")
+            const res = await api.fetchapi('http://180.149.241.208:3022/forgotPassword', 'post',
+                JSON.stringify({ "email": this.state.emailId }))
+            const result = await res.json();
+            console.log("api", result)
+            if (result.success === true) {
+                Alert.alert(result.message)
+                this.props.navigation.navigate('SetPassword', { otp: result.otp, token: result.token })
+            }
+            else {
+                Alert.alert(result.message)
+            }
         }
     }
-
-    // submit() {
-    //     const email = this.state.emaiId
-    //     console.warn('Hi', this.state.emailId)
-    //     // if ((!this.state.emaiId) && (this.state.email_err)) {
-    //     //     Alert.alert(" emailId  is required")
-    //     // }
-    //     // else {
-
-
-    //     fetch("http://180.149.241.208:3022/forgotPassword", {
-
-    //         // Adding method type 
-    //         method: "POST",
-
-    //         // Adding body or contents to send 
-    //         body: JSON.stringify({
-    //             "email": this.state.emailId
-    //         }
-    //         ),
-
-    //         // Adding headers to the request 
-    //         headers: {
-    //             "Content-type": "application/json; charset=UTF-8"
-    //         }
-    //     })
-
-    //         // Converting to JSON 
-    //         .then(response => response.json())
-
-    //         // Displaying results to console 
-    //         .then(json => {
-    //             let data = json
-    //             console.log(data)
-    //             console.log(data.success)
-
-    //             // {
-    //             //     data.success ?
-    //             //         this.props.navigation.navigate('SetPassword') :
-    //             //         Alert.alert(data.error_message)
-
-    //             // }
-    //             if (data.success === true) {
-    //                 this.props.navigation.navigate('SetPassword')
-    //             }
-    //             else {
-    //                 console.log(data.json, " g")
-    //                 Alert.alert("email_id is invalid")
-    //             }
-
-    //             // }
-    //         }
-    //         );
-
-
-
-
 
     render() {
         return (
             <View style={globalstyles.Container}>
-
                 <Text style={globalstyles.neostore_logo}>NeoSTORE</Text>
                 <Text style={globalstyles.Containerhead}>Forgot Password ?</Text>
                 <TextField placeholder="Enter Userid" name="user"
